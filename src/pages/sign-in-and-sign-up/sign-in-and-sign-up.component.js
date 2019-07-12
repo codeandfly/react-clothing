f6b239fc2c95acc0091fcc42400253f4ef44c08d
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-07-11 21:56:44 -0400

    Added SignIn and FormInput components + styling

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import SignIn from '../../components/sign-in/sign-in.component';
+
+// class SignInandSignUpPage extends Component {
+//   render() {
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+// }
+
+const SignInandSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn/>
+    </div>
+  )
+}
+
+export default SignInandSignUpPage;
