9510cf347792c803a95230156527a32ac7c9a2b2
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-07-11 22:12:27 -0400

    Added form button component and style

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -24,7 +25,7 @@ class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
-        <h2>I already have an accound</h2>
+        <h2 className="title">I already have an accound</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -44,7 +45,7 @@ class SignIn extends Component {
             label="password"
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );
