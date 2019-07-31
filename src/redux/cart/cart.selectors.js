1a9ea0a5aa84ad6b37a662da1f2d2e27e4ca5bfa
Louis <louis@Louiss-Mac-mini.local>
2019-07-31 19:28:35 -0400

    Added checkout page structure

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,4 +16,13 @@ export const selectCartItemsCount = createSelector(
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.hidden
-)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (acc, cartItem) => acc + cartItem.quantity * cartItem.price,
+      0
+    )
+);
