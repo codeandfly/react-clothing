6dc1c2b778cbfd9dfa766a5ca5147de5860a2833
Louis <louis@Louiss-Mac-mini.local>
2019-08-05 19:51:27 -0400

    Normalize shop data and modified selectCollection selector. Added selectCollectionsForPreview selector to convert our collections from an object into an array (so we can map through it in our CollectionPreview component

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -4,20 +4,22 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../preview-collection/collection-preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {collections.map(({ id, ...collections}) => (
-      <CollectionPreview key={id} {...collections} />
-    ))}
-  </div>
-);
+const CollectionsOverview = ({ collections }) => {
+  console.log(collections)
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...collections}) => (
+        <CollectionPreview key={id} {...collections} />
+      ))}
+    </div>
+)};
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview)
