951befa21bc7be1d327bf8d0a1972138ad3c6449
Louis <louis@Louiss-Mac-mini.local>
2019-09-14 17:58:08 -0400

    Added Clear Cart Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
@@ -25,4 +25,8 @@ export function* fetchCollectionStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionStart)]);
 }
